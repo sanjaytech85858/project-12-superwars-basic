c5f86cafdc05ccbf696f1fa4cb365baf61d20754
sanjaytech85858 <sanjaytech85858@gmail.com>
2021-08-05 11:52:16 +0530

    superwar2

--- src/app.js
+++ src/app.js
@@ -24,6 +24,15 @@ const PLAYERS = [
 // initialize players with image and strength
 const initPlayers = (players) => {
     let detailedPlayers = [];
+    for(let i=0;i<players.length;i++)
+    {
+        detailedPlayers[i] = {
+            name: players[i],
+            strength: getRandomStrength(),
+            image: "images/super-"+(i+1)+".png",
+            type: (i % 2 == 0) ? 'hero' : 'villain'
+        }
+    }
     // Create players using for loop
     // Type your code here
 
@@ -32,12 +41,27 @@ const initPlayers = (players) => {
 
 // getting random strength
 const getRandomStrength = () => {
+    var strength = Math.random()*100
+    return (Math.ceil(strength))
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
 }
 
 const buildPlayers = (players, type) => {
     let fragment = '';
+    for(let i=0;i<players.length;i++)
+    {
+       
+            player= `
+            <div class="player">
+                <img src="${players[i].image}" alt="">
+                <div class="name">${players[i].name}</div>
+                <div class="strength">${players[i].strength}</div>
+            </div>`;
+        if (players[i].type == type) {
+            fragment+=player
+        }
+    }
 
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
